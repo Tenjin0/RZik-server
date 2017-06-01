432d8d18ffa7d71b51ba2ca0bf565f98e1dbcc90
Tenji <patricepetit@hotmail.com>
2017-06-01 09:13:46 +0200

    update audiofiles

--- routes/audiofiles.js
+++ routes/audiofiles.js
@@ -1,9 +1,19 @@
-var express = require('express');
-var router = express.Router();
-var audioController = require('../controllers/audiofiles');
+const express = require('express');
+const router = express.Router();
+const audioController = require('../controllers/audiofiles');
+
+const multer = require('multer');
+
+const UPLOAD_PATH = 'uploads';
+const upload = multer({ dest: `${UPLOAD_PATH}/` }) // multer configuration
+;
+
 /* GET users listing. */
 router.get('/', audioController.index);
-router.post('/', audioController.create);
+router.post('/', upload.fields([
+    { name: 'cover', maxCount: 1 },
+    { name: 'audio_file', maxCount: 1 }
+]), audioController.create);
 router.get('/:id', audioController.view);
 router.post('/:id', audioController.update);
 
